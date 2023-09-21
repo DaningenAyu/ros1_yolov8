2b4ac88746938112024cde317b82091560b54da5
Ayu <108796833+DaningenAyu@users.noreply.github.com>
2023-09-21 17:27:04 +0900

    Add files via upload

--- yolov8_pkg/src/yolov8.py
+++ yolov8_pkg/src/yolov8.py
@@ -13,6 +13,7 @@ class YOLOv8_detect():
     #初期設定
     def __init__(self):
         self.model = YOLO("yolov8n.pt")
+        #self.model = YOLO("paper_bag_last.pt")
         rospy.init_node('detectYOLOv8n',anonymous=True)#ノードを立てる
         self.bridge = CvBridge()
         #Realsenseのカラー画像の情報取得(topicを取得)
